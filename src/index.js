ba54043ee27bbecea937af39f6e7f36015519a06
Brian <wamwangimathenge@gmail.com>
2022-12-08 10:06:58 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    showToDo(e.target.new_task_description.value)
+  })
 });
+
+function showToDo(task){
+  let deleteBtn = document.createElement('button')
+  let listTasks = document.createElement('p')
+  deleteBtn.style.margin = '8px'
+  deleteBtn.innerHTML = 'x'
+  deleteBtn.addEventListener('click', () => {
+    listTasks.remove()
+  })
+  listTasks.innerHTML = task
+  listTasks.appendChild(deleteBtn)
+  document.querySelector('#tasks').appendChild(listTasks)
+}
